2f6d9719ec90f34a8b18a09f2e389a2700b626c3
Ana Raquel Matos <anaraquelpmatos@gmail.com>
2022-03-17 00:25:23 -0300

    Layout Tela Inicial

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import TelaInicial from "../components/TelaInicial";
+
+function App() {
+    return (
+        <>
+            <TelaInicial />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/TelaInicial.js
@@ -0,0 +1,11 @@
+function TelaInicial() {
+    return (
+        <section>
+            <img src="assets/img/logo.png" alt="Logo ZapRecall" />
+            <h1>ZapRecall</h1>
+            <button>Iniciar Recall!</button>
+        </section>
+    );
+}
+
+export default TelaInicial;
